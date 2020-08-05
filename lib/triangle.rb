a44e6dbfcde528165800a616c7bc64e455835005
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-08-05 05:11:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
   def validate_triangle
     if @a <= 0 || @b <= 0 || @c <= 0
       raise TriangleError
-    elsif @a + @b < @c || @a + @c < @b || @b + @c < @a
+    elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
       raise TriangleError
     end
   end
